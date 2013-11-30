4f483c15467b196da2d3963478faf74488c208a4
imilka <pkzl22@gmail.com>
2013-11-30 17:35:58 +0400

    Fix database model

--- kickass/models.py
+++ kickass/models.py
@@ -42,7 +42,7 @@ class User(Base):
     password = Column(Text)
     money = Column(Float)
     mail = Column(Text)
-    targets = relationship('Target', backref='user')
+    #targets = relationship('Target')
 
 class Target(Base):
     __tablename__ = 'target'
@@ -51,7 +51,7 @@ class Target(Base):
     type = Column(Text)
     deadline = Column(DateTime)
     money = Column(Float)
-    source = Column(Integer, ForeignKey('User.id'))
-    user_id = Column(Integer, ForeignKey('User.id'))
+    #source = Column(Integer, ForeignKey('User.id'))
+    #user_id = Column(Integer, ForeignKey('User.id'))
 
 
