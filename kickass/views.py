24e144a2c16389dc4a54d1142b7770b647afbd06
Anton Griev <anton@griev.ru>
2013-11-30 20:09:28 +0400

    db_initialize by test data2

--- kickass/views.py
+++ kickass/views.py
@@ -10,9 +10,10 @@ from .models import (
 
 
 @view_config(route_name='home', renderer='templates/main.pt')
-def my_view(request):
+def home(request):
     try:
         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+
     except DBAPIError:
         return Response(conn_err_msg, content_type='text/plain', status_int=500)
     return {'one': one, 'project': 'kickass'}
